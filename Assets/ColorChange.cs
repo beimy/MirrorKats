58112b47f0f5e01c2275f77385a6b07a3e11862e
MergeMastaFlash <arbm159357@gmail.com>
2020-09-12 22:11:17 -0400

    Initial Commit

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+   void ChangeColor ()
+    {
+        GameObject myObject = 
+    }
+}

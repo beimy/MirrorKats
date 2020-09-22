9168ba09c67d78c3e262de576e11a357251e04cd
Aaron <arbm159357@gmail.com>
2020-09-22 19:55:04 -0400

    Color Change
    Prototype for reading touch controls into the scene

--- Assets/ColorChange.cs
+++ Assets/ColorChange.cs
@@ -4,8 +4,43 @@ using UnityEngine;
 
 public class ColorChange : MonoBehaviour
 {
-   void ChangeColor ()
+   void Update ()
     {
-        GameObject myObject = 
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    Color newColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
+
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    Color newColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
+
+                }
+            }
+        }
+
+#endif
+
     }
 }

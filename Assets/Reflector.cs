256d6bbab5b6765965ff3217fa67e2111f36b641
MergeMastaFlash <arbm159357@gmail.com>
2020-11-10 18:46:35 -0500

    Progress on Raycasting

---
+++ Assets/Reflector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reflector : MonoBehaviour
+{
+    public GameObject RaycastOrigin;
+    public int layerMask = 8;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ShootRay(RaycastOrigin);
+    }
+
+    void ShootRay(GameObject myOrigin)
+    {
+        myOrigin = RaycastOrigin;
+
+        RaycastHit hit;
+        
+        if (Physics.Raycast(myOrigin.transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
---
+++ Assets/Scripts/IMirrorKat.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMirrorKat
+{
+    void Initialize();
+
+    string Name { get; set; }
+
+}
